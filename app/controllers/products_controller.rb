a3423af8416062107a109a585b7c35bd833b44cf
diana spieser <dianaspieser@gmail.com>
2023-01-03 12:22:36 +0100

    header et footer ok, js type ok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR description ILIKE :query"
+      @products = Product.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @products = Product.all
+    end
   end
   def new
     @product = Product.new
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
