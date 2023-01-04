00538c2d4022032726f789fb098df98ec4332a53
diana spieser <dianaspieser@gmail.com>
2023-01-04 14:40:16 +0100

    ready pour la mise en ligne

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root to: "pages#home"
+  root to: "categories#index"
   get "about", to: "pages#about"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :categories
-  resources :products
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories, only: [:index, :show]
+  resources :products, only: [:index, :show]
 end
