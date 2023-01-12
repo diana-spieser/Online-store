1a35d6119e7d5f69c592bd47d168fe9563920aa9
diana spieser <dianaspieser@gmail.com>
2023-01-12 11:59:50 +0100

    contact form heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :contacts, only: [:new, :create]
+  get 'contacts/sent'
   root to: "categories#index"
   get "about", to: "pages#about"
   resources :categories, only: [:index, :show]
