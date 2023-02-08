a777f815c4491288293754032fcae7d01fe6b356
diana spieser <dianaspieser@gmail.com>
2023-02-08 10:22:11 +0100

    ok env

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :products, dependent: :destroy
+
+  def username
+    return self.email.split('@')[0]
+  end
 end
